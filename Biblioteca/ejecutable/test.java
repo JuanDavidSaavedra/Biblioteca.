bac69d30adc34c0bb65ad77d395966db166a6f41
Juan David Saavedra <102627981+JuanDavidSaavedra@users.noreply.github.com>
2022-07-15 00:49:52 -0500

    Add files via upload

--- Biblioteca/ejecutable/test.java
+++ Biblioteca/ejecutable/test.java
@@ -9,7 +9,7 @@ public class test
     public static void main (String[] args)
     {
         VentanaPrincipal venPrin = new VentanaPrincipal();
-        Libro model = new Libro("", "", "", 0, true);
+        Libro model = new Libro("", "", "", 0, false);
         Controlador miControlador = new Controlador(venPrin, model);
         
     }
